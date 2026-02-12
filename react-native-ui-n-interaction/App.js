9d2dee9f90489479e4b590ad0f27ad88afc9820a
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-12 17:59:23 +0800

    Done with fixed dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,5 +1,10 @@
+import FixedDimensionsScreen from './screens/FixedDimensionsScreen';
 import StyleScreen from './screens/StyleScreen';
 
 export default function App() {
   return <StyleScreen />;
+}
+
+export default function App() {
+  return <FixedDimensionsScreen />;
 }
---
+++ react-native-ui-n-interaction/screens/FixedDimensionsScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function FixedDimensionsScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text>Fixed 150 x 150</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#34a853',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
