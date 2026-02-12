58a531df561d4c87c7306e4103dc453d42bb20e0
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-12 23:35:54 +0800

    Done with fixed dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -3,3 +3,8 @@ import StyleScreen from './screens/StyleScreen';
 export default function App() {
   return <StyleScreen />;
 }
+import FixedDimensionsScreen from './screens/FixedDimensionsScreen';
+
+export default function App() {
+  return <FixedDimensionsScreen />;
+}
