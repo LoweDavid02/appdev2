cec7a4299699f14ea7c050f1e1ea020bf6639121
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-12 23:35:15 +0800

    Done with style section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,20 +1,5 @@
-import FixedDimensionsScreen from './screens/FixedDimensionsScreen';
-import FlexDimensionsScreen from './screens/FlexDimensionsScreen';
-import PercentageDimensionsScreen from './screens/PercentageDimensionsScreen';
 import StyleScreen from './screens/StyleScreen';
 
 export default function App() {
   return <StyleScreen />;
-}
-
-export default function App() {
-  return <FixedDimensionsScreen />;
-}
-
-export default function App() {
-  return <FlexDimensionsScreen />;
-}
-
-export default function App() {
-  return <PercentageDimensionsScreen />;
 }
