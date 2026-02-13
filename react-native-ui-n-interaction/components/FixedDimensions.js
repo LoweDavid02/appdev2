bea79eb53e3177a20eaae0d311ae4187e1b01df9
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-13 15:16:06 +0800

    Done with fixed dimensions section

---
+++ react-native-ui-n-interaction/components/FixedDimensions.js
@@ -0,0 +1,16 @@
+import { View, StyleSheet } from "react-native";
+
+export default function FixedDimensions() {
+  return (
+    <View style={styles.box} />
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: "blue",
+    margin: 20,
+  },
+});
