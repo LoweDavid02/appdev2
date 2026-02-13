2da946db68c68a82697c02f28e4dd186f64dd910
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-13 15:42:31 +0800

    Done with flex section

---
+++ react-native-flexbox/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ScrollView } from "react-native";
+
+// Import Flexbox section components
+import FlexExample from "./components/FlexExample";
+import FlexDirectionExample from "./components/FlexDirectionExample";
+import LayoutDirectionExample from "./components/LayoutDirectionExample";
+import JustifyContentExample from "./components/JustifyContentExample";
+import AlignItemsExample from "./components/AlignItemsExample";
+
+export default function App() {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      
+      {/* FLEX */}
+      <FlexExample />
+
+      {/* FLEX DIRECTION */}
+      <FlexDirectionExample />
+
+      {/* LAYOUT DIRECTION */}
+      <LayoutDirectionExample />
+
+      {/* JUSTIFY CONTENT */}
+      <JustifyContentExample />
+
+      {/* ALIGN ITEMS */}
+      <AlignItemsExample />
+
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+});
