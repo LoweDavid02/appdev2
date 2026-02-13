5961f2e27c0d4a820de7756451d1cba7bb48db8e
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-13 15:43:00 +0800

    Done with flex direction section

---
+++ react-native-flexbox/components/FlexDirectionExample.js
@@ -0,0 +1,23 @@
+import { View, StyleSheet } from "react-native";
+
+export default function FlexDirectionExample() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box} />
+      <View style={styles.box} />
+      <View style={styles.box} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+  },
+  box: {
+    width: 80,
+    height: 80,
+    backgroundColor: "purple",
+    margin: 5,
+  },
+});
