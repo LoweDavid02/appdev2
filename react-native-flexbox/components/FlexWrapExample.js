4abd4cc64da80d715e049006b6a4d37bf3391206
LoweDavid02 <louiecamarines21@gmail.com>
2026-02-16 15:50:19 +0800

    Done with flex wrap section

---
+++ react-native-flexbox/components/FlexWrapExample.js
@@ -0,0 +1,30 @@
+import { View, StyleSheet } from "react-native";
+
+export default function FlexWrapExample() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box} />
+      <View style={styles.box} />
+      <View style={styles.box} />
+      <View style={styles.box} />
+      <View style={styles.box} />
+      <View style={styles.box} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#95e1d3",
+    margin: 5,
+  },
+});
