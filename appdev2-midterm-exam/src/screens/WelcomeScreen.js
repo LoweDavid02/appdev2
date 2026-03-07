4eb7cae3bb28c196a46671ff13deb0972d22fc0f
LoweDavid02 <louiecamarines21@gmail.com>
2026-03-07 13:27:13 +0800

    Step 1: Copy the Welcome Screen Skeleton Code
    Step 2: Create the Main Container
    
    Step 3: Create the Top Section
    
    Step 4: Create the Middle Section (Image Area)
    
    Step 5: Create the Bottom Section
    
    Step 6: Create the Sign Up Button
    
    Step 7: Create the Login Text Link

---
+++ appdev2-midterm-exam/src/screens/WelcomeScreen.js
@@ -0,0 +1,93 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+export default function WelcomeScreen() {
+  return (
+    <View style={styles.container}>
+      {/* 1. Top Section */}
+      <View style={styles.topSection}>
+        <Text style={styles.title}>Let's Get Started!</Text>
+      </View>
+
+      {/* 2. Middle Section */}
+      <View style={styles.middleSection}>
+        <Image
+          source={require("./../../assets/welcome.png")}
+          style={styles.image}
+        />
+      </View>
+
+      {/* 3. Bottom Section */}
+      <View style={styles.bottomSection}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <View style={styles.loginContainer}>
+          <Text style={styles.footerText}>Already have an account? </Text>
+          <Text style={styles.loginLink}>Log In</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7D7AFF",
+    paddingTop: 80,
+    paddingHorizontal: 30,
+  },
+  topSection: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+  middleSection: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  bottomSection: {
+    flex: 1.5,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 40,
+  },
+  button: {
+    backgroundColor: "#FFCC00",
+    width: "100%",
+    paddingVertical: 18,
+    borderRadius: 15,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "black",
+  },
+  loginContainer: {
+    flexDirection: "row",
+  },
+  footerText: {
+    color: "white",
+    fontSize: 15,
+  },
+  loginLink: {
+    color: "#FFCC00",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
