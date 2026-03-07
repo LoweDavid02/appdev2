fcd0de6b85d389564e3c1175c5dfe8e0d9121b17
LoweDavid02 <louiecamarines21@gmail.com>
2026-03-07 13:40:39 +0800

    Step 1: Copy the Welcome Screen Skeleton Code

--- appdev2-midterm-exam/src/screens/WelcomeScreen.js
+++ appdev2-midterm-exam/src/screens/WelcomeScreen.js
@@ -5,7 +5,7 @@ export default function WelcomeScreen() {
     <View style={styles.container}>
       {/* 1. Top Section */}
       <View style={styles.topSection}>
-        <Text style={styles.title}>Let's Get Started!</Text>
+        <Text style={styles.title}></Text>
       </View>
 
       {/* 2. Middle Section */}
@@ -19,75 +19,18 @@ export default function WelcomeScreen() {
       {/* 3. Bottom Section */}
       <View style={styles.bottomSection}>
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Sign Up</Text>
+          <Text style={styles.buttonText}></Text>
         </TouchableOpacity>
 
         <View style={styles.loginContainer}>
-          <Text style={styles.footerText}>Already have an account? </Text>
-          <Text style={styles.loginLink}>Log In</Text>
+          <Text style={styles.footerText}></Text>
+          <TouchableOpacity>
+            <Text style={styles.loginLink}></Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#7D7AFF",
-    paddingTop: 80,
-    paddingHorizontal: 30,
-  },
-  topSection: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-    color: "white",
-    textAlign: "center",
-  },
-  middleSection: {
-    flex: 3,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
-  },
-  bottomSection: {
-    flex: 1.5,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 40,
-  },
-  button: {
-    backgroundColor: "#FFCC00",
-    width: "100%",
-    paddingVertical: 18,
-    borderRadius: 15,
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  buttonText: {
-    fontWeight: "bold",
-    fontSize: 18,
-    color: "black",
-  },
-  loginContainer: {
-    flexDirection: "row",
-  },
-  footerText: {
-    color: "white",
-    fontSize: 15,
-  },
-  loginLink: {
-    color: "#FFCC00",
-    fontWeight: "bold",
-    fontSize: 15,
-  },
-});
+const styles = StyleSheet.create({});
